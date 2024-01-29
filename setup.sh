c39ce5ef073857db7d5b81bf11bb3540ee935674
jb <jean-baptiste.huet@purecontrol.com>
2024-01-29 19:38:04 +0100

    ci: update ci and add implementation of link with vscode debugger and docker

--- setup.sh
+++ setup.sh
@@ -5,32 +5,38 @@
 #   INSTANCE_NAME : change the default docker compose instance name
 #   URL_PORT : change the default port to 3000
 
+# Check if docker-compose file already exist
+if [ -f docker-compose.yml ]; then
+    echo "/!\ ERROR : The docker-compose file already exist"
+    exit 1
+fi
+
 # Define default env vars
 INSTANCE_NAME="${USER}-$(basename $(pwd))"
 APP_VERSION=$(git describe --tags --match 'v[0-9]*.[0-9]*.[0-9]' --dirty --always)
-APP_PORT=3000
-REBOOK_IMAGE=pureapi:\${INSTANCE_NAME}
 PYTHON_VERSION=$(cat .python-version | head -n1)
 
-# Check if env file already exist
-if [ -f .env ]; then
-    echo "/!\ Warning : The env file already exist"
-else
-    export INSTANCE_NAME APP_VERSION APP_PORT
+# Generate .env file
+cat << EOF > .env.tmp
+INSTANCE_NAME=${INSTANCE_NAME}
 
-    # Generate .env file
-    cat deploy/env.example | envsubst > .env
+APP_VERSION=${APP_VERSION}
 
-    echo "Env file generate please fill it at .env"
-fi
+REBOOK_IMAGE=rebook:${INSTANCE_NAME}
 
-echo
+PYTHON_VERSION=${PYTHON_VERSION}
+
+EOF
+
+cat deploy/env >> .env.tmp
 
 # Construct docker compose
-docker compose -p "${INSTANCE_NAME}" --env-file .env --project-directory . -f ./deploy/docker-compose.yml -f ./deploy/docker-compose-dev-override.yml config > docker-compose.yml
+docker compose -p "${INSTANCE_NAME}" --env-file .env.tmp --project-directory . -f ./deploy/docker-compose.yml -f ./deploy/docker-compose-dev-override.yml config > docker-compose.yml
+
+rm .env.tmp
 
 echo "Instance ${INSTANCE_NAME} ready"
 echo "Your docker-compose.yml file is ready to use"
-echo "Once started, your installation will be available at localhost:${APP_PORT}"
+echo "Once started, your installation will be available at localhost:8000"
 echo
 echo "\`docker compose up -d\` to start it"
