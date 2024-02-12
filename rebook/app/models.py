a866bbb3e956c5dfe1ada0aa43f09133b3f183f9
jb <jean-baptiste.huet@purecontrol.com>
2024-02-12 22:37:04 +0100

    feat: end of django tutorial

--- rebook/app/models.py
+++ rebook/app/models.py
@@ -1,13 +1,20 @@
 # Create your models here.
-from django.db import models
+from datetime import timedelta
+
+from django.db.models import CASCADE, CharField, DateTimeField, ForeignKey, IntegerField, Model
+from django.utils import timezone
 
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
+class Question(Model):
+    question_text = CharField(max_length=200)
+    pub_date = DateTimeField("date published")
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - timedelta(days=1) <= self.pub_date <= now
 
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+class Choice(Model):
+    question = ForeignKey(Question, on_delete=CASCADE)
+    choice_text = CharField(max_length=200)
+    votes = IntegerField(default=0)
--- rebook/app/views.py
+++ rebook/app/views.py
@@ -1,33 +1,38 @@
-from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.http import HttpRequest, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-from django.template import loader
 from django.urls import reverse
+from django.utils import timezone
+from django.views.generic import DetailView as DjangoDetailView
+from django.views.generic import ListView
 
 from .models import Choice, Question
 
 
-# Create your views here.
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("index.html")
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+class IndexView(ListView):
+    template_name = "index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, "detail.html", {"question": question})
+class DetailView(DjangoDetailView):
+    model = Question
+    template_name = "detail.html"
 
 
-def vote(request, question_id):
+class ResultsView(DetailView):
+    model = Question
+    template_name = "results.html"
+
+
+def vote(request: HttpRequest, question_id: int):
     question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+        selected_choice: Choice = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
         return render(
@@ -45,8 +50,3 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse("results", args=(question.id,)))
-
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "results.html", {"question": question})
