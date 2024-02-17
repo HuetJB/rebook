efff4989a4fad23d4f257df146fa1642f72dded5
jb <jean-baptiste.huet@purecontrol.com>
2024-02-17 17:53:50 +0100

    feat: add profile page

--- rebook/accounts/views.py
+++ rebook/accounts/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, TemplateView
 
 from .forms import SignupForm
 
@@ -8,3 +10,8 @@ class SignUpView(CreateView):
     form_class = SignupForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+
+@method_decorator(login_required, name="dispatch")
+class ProfileView(TemplateView):
+    template_name = "accounts/profile.html"
