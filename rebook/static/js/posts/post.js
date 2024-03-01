564986b8d1310679b4d59d3b7579b58e9cb747d7
jb <jean-baptiste.huet@purecontrol.com>
2024-03-01 14:36:37 +0100

    feat: update style for post page

--- rebook/static/js/posts/post.js
+++ rebook/static/js/posts/post.js
@@ -1,13 +1,8 @@
 let favouriteButton = document.querySelector("#favouritesButton");
 
 if (favouriteButton) {
-    favouriteButton.changeTextToFavourite = function() {
-        favouriteButton.innerText = "Remove from favourites";
-    }
-
-    favouriteButton.changeTextToNotFavourite = function() {
-        favouriteButton.innerText = "Add to favourites";
-    }
+    favouriteButton.changeTextToFavourite = () => favouriteButton.innerText = "Remove from favourites";
+    favouriteButton.changeTextToNotFavourite = () => favouriteButton.innerText = "Add to favourites";
 
     favouriteButton.onclick = function() {
         fetch(favouriteButton.getAttribute("url"), {
@@ -19,12 +14,34 @@ if (favouriteButton) {
         })
         .then(response => response.json())
         .then(data => {
-            if (data.favourite_status) {
-                favouriteButton.changeTextToFavourite();
-            }
-            else {
-                favouriteButton.changeTextToNotFavourite();
-            }
+            if (data.favourite_status) favouriteButton.changeTextToFavourite();
+            else favouriteButton.changeTextToNotFavourite();
         })
     }
 }
+
+let imageIndex = 0;
+let images = document.querySelectorAll("div#images img");
+let previousButton = document.querySelector("div#images button#previous");
+let nextButton = document.querySelector("div#images button#next");
+
+function showImage(index) {
+    if (index < 0 || index >= images.length) return;
+
+    imageIndex = index;
+
+    if (imageIndex <= 0) previousButton.style.display = "none";
+    else previousButton.style.display = "block";
+
+    if (imageIndex >= images.length - 1) nextButton.style.display = "none";
+    else nextButton.style.display = "block";
+
+    images.forEach(image => image.style.display = "none");
+
+    images[imageIndex].style.display = "block";
+}
+
+showImage(imageIndex);
+
+previousButton.onclick = () => showImage(imageIndex - 1);
+nextButton.onclick = () => showImage(imageIndex + 1);
